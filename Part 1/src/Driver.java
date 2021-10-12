ca4232e60adee77e19eb9ede25a2fc3ddb6e0438
dashIink <71524804+dashIink@users.noreply.github.com>
2021-10-11 22:48:13 -0400

    Part 1 Completed

--- Part 1/src/Driver.java
+++ Part 1/src/Driver.java
@@ -1,15 +1,17 @@
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class Driver {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input;
         Keyboard k = new Keyboard();
         Scanner c = new Scanner();
         System.out.println("Please enter a UPC");
-        input = System.console().readLine();
+        input = br.readLine();
         k.setUPCCode(Integer.parseInt(input));
         System.out.println("Please enter a UPC");
-        input = System.console().readLine();
+        input = br.readLine();
         c.scannedUPCCode(Integer.parseInt(input));
     }
 }
